09843a3dfe27ee68864cc7d32a5cf872dfb54378
fmaulll <maul2821@gmail.com>
2023-06-19 22:37:12 +0700

    main component & api calling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,163 @@
-import Image from 'next/image'
+"use client";
+import Filter from "@/components/Filter";
+import Image from "next/image";
+import axios from "axios";
+import React, {
+  Fragment,
+  useCallback,
+  useContext,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import Pokemon from "@/components/Pokemon";
+import { FilterTypes, PokemonTypeList, PokemonTypes } from "../type";
+
+type AllPokemonType = {
+  name: string;
+  url: string;
+};
 
 export default function Home() {
+  const [hasMore, setHasMore] = useState<boolean>(false);
+  const [offset, setOffset] = useState<number>(0);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [allPokemon, setAllPokemon] = useState<PokemonTypes[]>([]);
+  const [openDialog, setOpenDialog] = useState<boolean>(false);
+  const [chosenPokemon, setChosenPokemon] = useState<PokemonTypes>({
+    name: "",
+    height: 0,
+    id: 0,
+    image: "",
+    types: "",
+    weight: 0,
+  });
+  const [typeList, setTypeList] = useState<PokemonTypeList[]>([]);
+  const [filter, setFilter] = useState<FilterTypes>({
+    name: "",
+    type: "",
+  });
+  const observer = useRef();
+  const lastPokemonElement = useCallback((node: any) => {}, [hasMore]);
+
+  const getPokemon = async (pokemon: AllPokemonType) => {
+    try {
+      const result = await axios.get(pokemon.url);
+      if (result.status === 200) {
+        const { data } = result;
+        setAllPokemon((prev) => {
+          return [
+            ...prev,
+            {
+              name:
+                data.name[0].toUpperCase() +
+                data.name.slice(1, data.name.length),
+              image: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${data.id}.png`,
+              height: data.height,
+              weight: data.weight,
+              id: data.id,
+              types: data.types[0].type.name,
+            },
+          ];
+        });
+        setLoading(false);
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  const getAllPokemon = async () => {
+    setLoading(true);
+    try {
+      const result = await axios.get(
+        `https://pokeapi.co/api/v2/pokemon?offset=${offset}`
+      );
+      if (result.status === 200) {
+        const { results } = result.data;
+        results.map((item: AllPokemonType, index: number) => {
+          getPokemon(item);
+        });
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  const getAllPokemonType = async () => {
+    setLoading(true);
+    try {
+      const result = await axios.get("https://pokeapi.co/api/v2/type");
+      if (result.status === 200) {
+        const { results } = result.data;
+        setTypeList(results);
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
+  const filterPokemon = (array: PokemonTypes[]) => {
+    return array
+      .filter((pokemon) => pokemon.name.toLowerCase().includes(filter.name))
+      .filter((pokemon) => pokemon.types.toLowerCase().includes(filter.type));
+  };
+
+  const handleClickAddPokemon = (data: PokemonTypes) => {
+    setChosenPokemon(data);
+    handleOpenDialog();
+    // addPokemon({ ...data, alias: "" });
+  };
+
+  const handleAddPokemon = (data: PokemonTypes, alias: string) => {
+    if (!alias) {
+      return;
+    }
+  };
+
+  const handleOpenDialog = () => {
+    setOpenDialog(!openDialog);
+  };
+
+  const handleChangeFilter = (filter: FilterTypes) => {
+    setFilter(filter);
+  };
+
+  const handleResetFilter = () => {
+    setFilter({
+      name: "",
+      type: "",
+    });
+  };
+
+  useEffect(() => {
+    getAllPokemon();
+    getAllPokemonType();
+  }, []);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+    <main className="px-8 py-4 bg-gray-800">
+      <Filter
+        onResetFilter={handleResetFilter}
+        typeList={typeList}
+        onSubmitFilter={handleChangeFilter}
+      />
+      <div className="grid grid-cols-6 gap-4">
+        {filterPokemon(allPokemon).map((item, index) => {
+          if (allPokemon.length === index + 1) {
+            return (
+              <div>
+                <Pokemon data={item} key={index} />
+              </div>
+            );
+          } else {
+            return (
+              <div ref={lastPokemonElement}>
+                <Pokemon data={item} key={index} />
+              </div>
+            );
+          }
+        })}
       </div>
     </main>
-  )
+  );
 }
